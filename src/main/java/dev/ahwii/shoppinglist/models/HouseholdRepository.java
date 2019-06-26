fce32bf519b377006ca6a47d7ef3e2977c0a9e36
thomaslienbacher <lienbacher.tom@gmail.com>
2019-06-26 10:48:08 +0200

    Added household model

---
+++ src/main/java/dev/ahwii/shoppinglist/models/HouseholdRepository.java
@@ -0,0 +1,7 @@
+package dev.ahwii.shoppinglist.models;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface HouseholdRepository extends CrudRepository<Household, Integer> {
+
+}
