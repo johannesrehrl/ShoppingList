8f93489e3f7d423ca79be8af79f76eed50051c8e
ElvirHodzic <elvir.hodzic.student@htl-hallein.at>
2019-06-27 08:43:39 +0200

    Removed category model

--- src/main/java/dev/ahwii/shoppinglist/models/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.ahwii.shoppinglist.models;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Integer> {
-
-}
